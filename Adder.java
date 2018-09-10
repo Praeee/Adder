229c3ef594bf51bd99bc286dbf988edec82dfd7a
Prae <60050211@kmitl.ac.th>
2018-09-10 18:47:54 +0700

    add : adder.java

---
+++ Adder.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Adder{
+   public static void main (String[]args) {
+    Scanner sc = new Scanner (System.in);   
+    System.out.print ("x : ");
+       int x = sc.nextInt();
+       System.out.print ("y : ");
+       int y = sc.nextInt();
+       int z = x+y;
+       System.out.print ("sum : "+z);
+
+   }
+
+}
+
