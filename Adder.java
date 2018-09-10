51b34895194830251e7e1a171884ec52a150b70b
Prae <60050211@kmitl.ac.th>
2018-09-10 19:07:03 +0700

    edit sop x

--- Adder.java
+++ Adder.java
@@ -2,7 +2,7 @@ import java.util.*;
 class Adder{
    public static void main (String[]args) {
     Scanner sc = new Scanner (System.in);   
-    System.out.print ("x : ");
+       System.out.print ("x : ");
        int x = sc.nextInt();
        System.out.print ("y : ");
        int y = sc.nextInt();
